a870ce76847cddce717b0b2d16643861800de723
Barend Erasmus <barend.erasmus@euromonitor.com>
2019-01-28 13:21:57 +0200

    add fab

--- src/Pages/LoyaltyCards.js
+++ src/Pages/LoyaltyCards.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import { LoyaltyCard } from '../Components';
+import { Fab } from '@material-ui/core';
+import { Add } from '@material-ui/icons';
 
 export class LoyaltyCards extends React.Component {
   state = {
@@ -12,10 +14,19 @@ export class LoyaltyCards extends React.Component {
         id: 2,
         date: new Date(2019, 4, 22),
       },
+      {
+        id: 3,
+        date: new Date(2019, 7, 17),
+      },
     ],
   };
 
   render() {
-    return this.state.loyaltyCards.map((loyaltyCard) => <LoyaltyCard key={loyaltyCard.id} loyaltyCard={loyaltyCard} />);
+    return (<div>
+      {this.state.loyaltyCards.map((loyaltyCard) => <LoyaltyCard key={loyaltyCard.id} loyaltyCard={loyaltyCard} />)}
+      <Fab color="primary" style={{bottom: '10px', position: 'fixed', right: '10px'}}>
+        <Add />
+      </Fab>
+    </div>)
   }
 }
