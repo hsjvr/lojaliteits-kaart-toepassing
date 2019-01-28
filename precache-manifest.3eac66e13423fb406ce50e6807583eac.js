0779f02ff28fa84d2f00287a3cc538ef536b9c5a
Barend Erasmus <barend.erasmus@euromonitor.com>
2019-01-28 12:42:40 +0200

    Updates

---
+++ precache-manifest.3eac66e13423fb406ce50e6807583eac.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "5f0aa8149d4a99807d9b",
+    "url": "/lojaliteits-kaart-toepassing/static/js/runtime~main.5f0aa814.js"
+  },
+  {
+    "revision": "2cc330607bdf2b701fa5",
+    "url": "/lojaliteits-kaart-toepassing/static/js/main.2cc33060.chunk.js"
+  },
+  {
+    "revision": "9de769d512a1291c8b81",
+    "url": "/lojaliteits-kaart-toepassing/static/js/1.9de769d5.chunk.js"
+  },
+  {
+    "revision": "2cc330607bdf2b701fa5",
+    "url": "/lojaliteits-kaart-toepassing/static/css/main.58af56d4.chunk.css"
+  },
+  {
+    "revision": "f26eb30b92180d1577cff6b3eb9d1f56",
+    "url": "/lojaliteits-kaart-toepassing/index.html"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/lojaliteits-kaart-toepassing/precache-manifest.3eac66e13423fb406ce50e6807583eac.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/lojaliteits-kaart-toepassing/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+});
