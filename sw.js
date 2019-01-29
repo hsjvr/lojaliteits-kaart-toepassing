6774e27e3b715d1af6f213c57ee83b43a3238c68
Barend Erasmus <barend.erasmus@euromonitor.com>
2019-01-29 07:13:15 +0200

    Updates

--- sw.js
+++ sw.js
@@ -2,4 +2,13 @@ self.addEventListener('install', function(event) {});
 
 self.addEventListener('activate', function(event) {});
 
-self.addEventListener('fetch', function(event) {});
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches
+      .open(cacheName)
+      .then((cache) => cache.match(event.request, { ignoreSearch: true }))
+      .then((response) => {
+        return response || fetch(event.request);
+      }),
+  );
+});
